1badf84447b6745e15c89847ac56fc72bf099df9
unknown <motunrayodusina@gmail.com>
2023-07-09 23:06:21 +0100

    project

--- src/Hooks/Switcher.jsx
+++ src/Hooks/Switcher.jsx
@@ -1,26 +1,24 @@
 import { useState } from "react";
 import { DarkModeSwitch } from "react-toggle-dark-mode";
 import useDarkSide from "./useDarkSide";
-  
+
 export default function Switcher() {
-    const [colorTheme, setTheme] = useDarkSide();
-    const [darkSide, setDarkSide] = useState(
-        colorTheme === "light" ? true : false
-    );
-  
-    const toggleDarkMode = (checked) => {
-        setTheme(colorTheme);
-        setDarkSide(checked);
-    };
-  
-    return (
-        <>
-            <DarkModeSwitch
-                style={{ marginBottom: "0.5rem" }}
-                checked={darkSide}
-                onChange={toggleDarkMode}
-                size={30}
-            />
-        </>
-    );
+  const [colorTheme, setTheme] = useDarkSide();
+  const [darkSide, setDarkSide] = useState(true); // Set dark mode as the default
+
+  const toggleDarkMode = (checked) => {
+    setTheme(checked ? "dark" : "light");
+    setDarkSide(checked);
+  };
+
+  return (
+    <>
+      <DarkModeSwitch
+        style={{ marginBottom: "0.5rem" }}
+        checked={darkSide}
+        onChange={toggleDarkMode}
+        size={30}
+      />
+    </>
+  );
 }
